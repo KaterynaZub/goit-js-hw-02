cf5974eec89b84f81794d951d5a94ec76a0b17aa
KaterynaZub <katekornzub@gmail.com>
2023-11-21 00:03:03 +0100

    add new

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,20 @@
+function checkForSpam(message) {
+    if (message) {
+        
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
